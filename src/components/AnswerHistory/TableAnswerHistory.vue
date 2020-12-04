a1aca18711f24399a5a8c959b35e26805851fdfe
Jérémy <pepin.jeremy81@gmail.com>
2020-12-04 10:08:19 +0100

    feat(answer-history): en-tête fixe

--- src/components/AnswerHistory/TableAnswerHistory.vue
+++ src/components/AnswerHistory/TableAnswerHistory.vue
@@ -1,6 +1,6 @@
 <template>
     <q-dialog v-model="$attrs.value" v-bind="$attrs" v-on="$listeners" full-width>
-        <q-card :style="`width: ${500 * players.length}px !important;`">
+        <q-card :style="`width: ${500 * players.length}px !important;`" class="tableFixHead">
             <q-markup-table bordered dense separator="cell" wrap-cells>
                 <thead v-if="!(players.length === 1 && players[0].userId === me.id)">
                     <tr>
@@ -102,7 +102,20 @@ export default class TableAnswerHistory extends Mixins(UserMixin) {
 </script>
 
 <style scoped>
-    table td, table th {
+    th {
         width: 500px;
+    }
+
+    td {
+        width: 655px;
+    }
+
+    table tbody, table thead {
+        display: block;
+    }
+
+    table tbody {
+        overflow: auto;
+        height: 500px;
     }
 </style>
